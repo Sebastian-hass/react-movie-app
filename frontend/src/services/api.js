b6ab891b474e96fe6abd0a94aa7ee569163d9f82
Sebastian <jesussebastianalonsoarias@gmail.com>
2025-06-25 20:40:25 +0200

    Subiendo proyecto de React a GitHub

---
+++ frontend/src/services/api.js
@@ -0,0 +1,18 @@
+const API_KEY = "b1a97bcbca46bfa1fc3832655755a9ae";
+const BASE_URL = "https://api.themoviedb.org/3";
+
+export const getPopularMovies = async () => {
+  const response = await fetch(`${BASE_URL}/movie/popular?api_key=${API_KEY}`);
+  const data = await response.json();
+  return data.results;
+};
+
+export const searchMovies = async (query) => {
+  const response = await fetch(
+    `${BASE_URL}/search/movie?api_key=${API_KEY}&query=${encodeURIComponent(
+      query
+    )}`
+  );
+  const data = await response.json();
+  return data.results;
+};
